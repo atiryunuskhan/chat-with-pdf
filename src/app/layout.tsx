d238f438b17e856fc5f0f040d0856b269a972a2d
atiryunuskhan <atiryunuskhan945@gmail.com>
2024-11-22 01:42:56 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,15 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-
-    <ClerkProvider>
-      <Providers>
-        <html lang="en">
-          <body className={inter.className}>{children}</body>
-          <Toaster />
-        </html>
-      </Providers>
-    </ClerkProvider>
-
+    <html lang="en">
+      <body className={inter.className}>
+        <ClerkProvider>
+          <Providers>
+            {children}
+            <Toaster />
+          </Providers>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
